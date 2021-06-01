e03d76e144659651ee437b68f8defc39389efe84
otis wilcox <otiswilcox@gmail.com>
2021-06-01 10:43:57 -0400

    pushed up my js 101 exercises

---
+++ exercises/level1.js
@@ -0,0 +1,44 @@
+function lvl1exercise1() {
+  // Declare a variable without instantiating it and return it!
+  const age;
+  return age;
+}
+
+function lvl1exercise2() {
+  // Declare and instantiate a number and return it
+  const myAge = 30;
+  return myAge;
+}
+
+function lvl1exercise3() {
+  // Declare and instantiate a floating point number that is not a whole number and return it
+  const ex3 = 3.33;
+  return ex3;
+}
+
+function lvl1exercise4() {
+  // Declare and instantiate a string "Hello World!" and return it
+  const greeting = "Hello World!";
+  return greeting;
+}
+
+function lvl1exercise5() {
+  // Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
+  const array = ["Hello World!", 4];
+  return array;
+}
+
+function lvl1exercise6() {
+  // Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
+  const object1 = {
+    key1: "Hello World!",
+    key2: 4
+  }
+  return object1;
+}
+
+function lvl1exercise7() {
+  // Declare and instantiate a boolean value 'false' and return it
+  const running = false;
+  return running;
+}
